08b6eb40f9d0d9cee65d1520aa86b56fbf1dbbf6
batuhan.karadere <batuhan.karadere@EMREOZEN>
2024-10-22 16:26:53 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7005");
+var greetClient = new Greeter.GreeterClient(channel);
+
+
+HelloReply helloReply =  await greetClient.SayHelloAsync(new HelloRequest 
+{ 
+    Name = "Berk" 
+});  
+
+
+Console.WriteLine(helloReply.Message);
+
+
+Console.ReadLine();
